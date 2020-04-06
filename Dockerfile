6046e14ad7fe2dac183fc9682992786f808f1e71
Bilal TAS <bilaltas@me.com>
2020-04-06 08:28:08 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:latest
+
+ENV APP_ROOT /landing
+
+RUN mkdir ${APP_ROOT}
+WORKDIR ${APP_ROOT}
+ADD . ${APP_ROOT}
+
+RUN npm install
+RUN npm run build
+
+ENV HOST 0.0.0.0
+ENV PORT 3000
---
+++ src/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div>
+
+      <logo />
+      <h1 class="title">Revisionary App</h1>
+      <h2 class="subtitle">Revisionary App Landing Page</h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >Documentation</a>
+        <nuxt-link class="button--grey" to="contact">Contact</nuxt-link>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+</style>
