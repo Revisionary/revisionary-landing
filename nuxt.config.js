fdc631501d8c928209b44d0b85a10259f8fe80e2
Atakan Öz <atakanozofficial@gmail.com>
2020-05-31 18:50:26 +0300

    smooth scroll added

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,12 @@ export default {
   },
 	router: {
 		prefetchLinks: false,
+    scrollBehavior(to) {
+      if (to.hash) {
+        return window.scrollTo({ top: document.querySelector(to.hash).offsetTop + window.innerHeight, behavior: 'smooth' });
+      }
+      return window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
 	},
   /*
   ** Customize the progress-bar color
